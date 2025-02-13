2443e6e5750d832afc6bf5a06eace1174773232b
Natan Silva <natanssilva10@gmail.com>
2025-02-13 20:39:36 -0300

    FIX | Teste

--- app/src/views/UnderConstruction.vue
+++ app/src/views/UnderConstruction.vue
@@ -4,6 +4,7 @@
       <h1 class="text-3xl md:text-4xl font-bold mb-4 underline">Site em andamento!!</h1>
       <p class="text-lg md:text-2xl underline">Estou trabalhando para apresentar algo incrível. Volte em breve!</p>
       <p class="text-lg md:text-2xl mt-2 underline">
+        TESTE
         Fique de olho em atualizações no meu
         <a href="https://github.com/Natanssilva" target="_blank" rel="noopener noreferrer" class="text-blue-400 hover:text-blue-300 underline">Github</a>
       </p>
